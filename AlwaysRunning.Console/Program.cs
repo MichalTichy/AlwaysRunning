c2b36329171377cf90a3cc5d11053539c36d2b74
Michal Tichý <michal.tichy@riganti.cz>
2019-07-31 09:39:45 +0200

    Add project files.

---
+++ AlwaysRunning.Console/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlwaysRunning.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AlwaysRunning().Start();
+            Thread.Sleep(-1);
+        }
+    }
+}
